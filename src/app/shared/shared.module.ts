1fd1b7f6dbafdf2d6b744535b4a629bea656ef9a
Vincze Norbert <vinczenorbi04@gmail.com>
2025-12-19 19:23:24 +0200

    map added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShadowBoxComponent } from './shadow-box/shadow-box.component';
 import { MainDivComponent } from './main-div/main-div.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [ShadowBoxComponent, MainDivComponent],
+  declarations: [ShadowBoxComponent, MainDivComponent, MapComponent],
   imports: [CommonModule],
-  exports: [ShadowBoxComponent, MainDivComponent],
+  exports: [ShadowBoxComponent, MainDivComponent, MapComponent],
 })
 export class SharedModule {}
