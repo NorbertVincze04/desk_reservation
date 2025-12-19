8a172e3c5bfed545d3beecea39eea50afb680f41
Vincze Norbert <vinczenorbi04@gmail.com>
2025-12-19 18:25:57 +0200

    main-div-shared

---
+++ src/app/shared/main-div/main-div.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-div',
+  templateUrl: './main-div.component.html',
+  styleUrl: './main-div.component.css'
+})
+export class MainDivComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShadowBoxComponent } from './shadow-box/shadow-box.component';
+import { MainDivComponent } from './main-div/main-div.component';
 
 @NgModule({
-  declarations: [ShadowBoxComponent],
+  declarations: [ShadowBoxComponent, MainDivComponent],
   imports: [CommonModule],
-  exports: [ShadowBoxComponent],
+  exports: [ShadowBoxComponent, MainDivComponent],
 })
 export class SharedModule {}
