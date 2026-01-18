7eeb0c113c3ef28a08ec54e0acdbd18fb08df6e4
Vincze Norbert <vinczenorbi04@gmail.com>
2026-01-18 13:52:17 +0200

    desk status

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrl: './map.component.css',
 })
 export class MapComponent {
+  officeGroups = ['A', 'B', 'C', 'D'];
+  deskNumbers = [1, 2, 3, 4];
 
+  deskStatusMap: { [key: string]: string } = {
+    A3: 'booked',
+    B1: 'booked',
+    B2: 'booked',
+  };
+
+  getStatus(groupId: string, deskNum: number): string {
+    const id = groupId + deskNum;
+    return this.deskStatusMap[id] || 'available';
+  }
+
+  toggleDesk(groupId: string, deskNum: number) {
+    const id = groupId + deskNum;
+    const currentStatus = this.getStatus(groupId, deskNum);
+
+    if (currentStatus === 'available') {
+      this.resetSelections();
+      this.deskStatusMap[id] = 'selected';
+    } else if (currentStatus === 'selected') {
+      this.deskStatusMap[id] = 'available';
+    }
+  }
+
+  resetSelections() {
+    for (const deskId in this.deskStatusMap) {
+      if (this.deskStatusMap[deskId] === 'selected') {
+        this.deskStatusMap[deskId] = 'available';
+      }
+    }
+  }
 }
